1e4476f39ce95cd5648a824f1bab79402772ae95
Nelly Arutyunyan <ar.nelli.jw@gmail.com>
2026-07-21 12:57:10 +0400

    Day 2: Add if-else practice programs

---
+++ Python/Day 2/pass_or_fail.py
@@ -0,0 +1,6 @@
+question = "What score did you get on your test? "
+score = int(input(question))
+if score >= 50:
+    print("Congratulations! You passed the test!")
+else:
+    print("Sorry, you failed the test. Better luck next time!")
