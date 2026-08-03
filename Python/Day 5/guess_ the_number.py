8559135730bdbfa024a34f83526b0fa4a36d793a
Nelly Arutyunyan <ar.nelli.jw@gmail.com>
2026-08-03 09:46:22 +0400

    Day 5: Programming myself without any hunts

--- Python/Day 5/guess_ the_number.py
+++ Python/Day 5/guess_ the_number.py
@@ -5,14 +5,12 @@ print("Guess the number between 1 and 10")
 while guess != secret_number and attempts < 5:
     guess = int(input("Enter your guess: "))
     attempts += 1
-    print(f"You have {5 - attempts} attempts left.")
-    if guess == secret_number:
-        print(f"Congratulations! You guessed the number in {attempts} attempts!")
-        break
-    elif guess < secret_number:
+    if guess < secret_number:
         print("Your guess is too low. Try again.")
-    else:
+        print(f"You have {5 - attempts} attempts left.")
+    elif guess > secret_number:
         print("Your guess is too high. Try again.")
+        print(f"You have {5 - attempts} attempts left.")
 if guess == secret_number:
     print(f"Congratulations! You guessed the number in {attempts} attempts!")
 else:
